21deb4e250faa488212f5b0805f1af715984526f
Anderson Souza <andersom.eugenio@gmail.com>
2018-08-09 13:28:19 -0300

    adicionado particles e outras alterações de ui

--- js/main.js
+++ js/main.js
@@ -138,3 +138,8 @@ var main = {
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', main.init);
+$(function(){
+  particlesJS.load('particles-js', '/particles-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+})
